614907c18297c9875d98147372874e76192b66c2
Matias Gomez <matias.gomez@valkimia.com>
2021-03-09 19:40:04 -0300

    edited endpoints

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const app = express(),
   randomId = require("random-id");
@@ -6,75 +7,39 @@ bodyParser = require("body-parser");
 port = 3080;
 
 const idlen = 10;
-// place holder for the data
-const users = [];
+const heroes = [];
 let tasks = [];
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../ui/build")));
 
-app.get("/api/users", (req, res) => {
-  console.log("api/users called!");
-  res.json(users);
+app.get("/api/heroes", (req, res) => {
+  console.log("api/heroes called!");
+  res.json(heroes);
 });
 
-app.post("/api/user", (req, res) => {
-  const user = req.body.user;
-  console.log("Adding user:::::", user);
-  users.push(user);
-  res.json("user addedd");
-});
-
-app.post("/api/login", (req, res) => {
-  const user = req.body.user;
-  console.log("Adding user:::::", user);
-  const usersFound = users.filter(
-    (usr) => usr.email === user.email && usr.password === user.password
-  );
-  if (usersFound.length > 0) {
-    res.json({ status: true, message: "user found" });
-  } else {
-    res.json({ status: false, message: "user not found" });
-  }
-});
-
-app.post("/api/signup", (req, res) => {
-  const user = req.body.user;
-  console.log("Adding user:::::", user);
-  users.push(user);
-  res.json({ status: true, numberOfUsers: users.length });
-});
-
-app.get("/api/tasks", (req, res) => {
-  res.json(tasks);
-});
-
-app.post("/api/task", (req, res) => {
-  const task = req.body.task;
+app.post("/api/hero", (req, res) => {
+  const hero = req.body.hero;
   const id = randomId(idlen);
-  task.id = id;
-  tasks.push(task);
-  res.json({ status: true, message: `task ${task.id} addedd` });
+  hero.id = id;
+  heroes.push(hero);
+  res.json({ status: true, message: `hero ${hero.id} addedd` });
 });
 
-app.delete("/api/task/:id", (req, res) => {
-  console.log("deleting task:::", req.params.id);
+app.delete("/api/hero/:id", (req, res) => {
+  console.log("deleting hero:::", req.params.id);
   const id = req.params.id;
-  tasks = tasks.filter((tsk) => tsk.id !== id);
-  res.json({ status: true, message: `task ${id} deleted` });
+  heroes = heroes.filter((hero) => hero.id !== id);
+  res.json({ status: true, message: `hero ${id} deleted` });
 });
 
-app.put("/api/task", (req, res) => {
-  const task = req.body.task;
-  tasks = tasks.map((tsk) => {
-    if (tsk.id === task.id) tsk = task;
-    return tsk;
+app.put("/api/hero", (req, res) => {
+  const hero = req.body.task;
+  heroes = heroes.map((hro) => {
+    if (hro.id === hero.id) hro = hero;
+    return hro;
   });
-  res.json({ status: true, message: `task ${task.id} edited` });
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../ui/build/index.html"));
+  res.json({ status: true, message: `hero ${hero.id} edited` });
 });
 
 app.listen(port, () => {
--- ui/src/app/services/heroes.service.ts
+++ ui/src/app/services/heroes.service.ts
@@ -1,14 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Hero } from '../models/hero.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
+  apiURL = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
   getHeroes() {
-    return this.http.get<Hero[]>('/assets/mock/heroes.json');
+    return this.http.get<Hero[]>(`${this.apiURL}/heroes`);
   }
+
+  getHero(id: number) {
+    return this.http.get<Hero>(`${this.apiURL}/hero/${id}`);
+  }
+
+  addHero() {}
+
+  editHero(id: number) {}
+
+  deleteHero(id: number) {}
 }
