74958b66bee37ee48893ffaa95c49079ffc4bce7
Matias Gomez <matias.gomez@valkimia.com>
2021-03-09 21:24:08 -0300

    Added edit and delete functionality fixes in api

--- ui/src/app/pages/heroes-list/heroes-list.component.ts
+++ ui/src/app/pages/heroes-list/heroes-list.component.ts
@@ -3,6 +3,8 @@ import { Hero } from 'src/app/models/hero.model';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { HeroesService } from 'src/app/services/heroes.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ApiResponse } from 'src/app/models/api-response.model';
 
 @Component({
   selector: 'app-heroes-list',
@@ -10,6 +12,8 @@ import { HeroesService } from 'src/app/services/heroes.service';
   styleUrls: ['./heroes-list.component.scss'],
 })
 export class HeroesListComponent implements OnInit, AfterViewInit {
+  emptyDataMessage: string = 'Loading heroes...';
+
   displayedColumns: string[] = [
     'id',
     'name',
@@ -26,12 +30,30 @@ export class HeroesListComponent implements OnInit, AfterViewInit {
   constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
-    this.heroesService.getHeroes().subscribe((heroes) => {
-      this.dataSource.data = heroes;
-    });
+    this.heroesService.getHeroes().subscribe(
+      (heroes: Hero[]) => {
+        if (heroes.length == 0) {
+          this.emptyDataMessage = 'No heroes to show! Add a new one!';
+        }
+
+        this.dataSource.data = heroes;
+      },
+      (error: HttpErrorResponse) => {
+        //TODO: implementar alerta para que el usuario se entere
+        this.emptyDataMessage =
+          'A problem happens when fetching heroes from the server. Try to refresh the page';
+      }
+    );
   }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+  }
+
+  public deleteHero(heroId: string) {
+    this.heroesService.deleteHero(heroId).subscribe(
+      (response: ApiResponse) => {},
+      (error: HttpErrorResponse) => {}
+    );
   }
 }
