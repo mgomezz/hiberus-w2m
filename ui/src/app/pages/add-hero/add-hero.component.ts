af44c88bcdf2ce0224e1ce519f0742fb2a96cdc1
Matias Gomez <matias.gomez@valkimia.com>
2021-03-11 18:23:20 -0300

    fixed add hero component

--- ui/src/app/pages/add-hero/add-hero.component.ts
+++ ui/src/app/pages/add-hero/add-hero.component.ts
@@ -1,10 +1,17 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Hero } from 'src/app/models/hero.model';
 import { Location } from '@angular/common';
 import { HeroesService } from 'src/app/services/heroes/heroes.service';
+import { ApiResponse } from 'src/app/models/api-response.model';
 
 @Component({
   selector: 'app-add-hero',
@@ -12,15 +19,11 @@ import { HeroesService } from 'src/app/services/heroes/heroes.service';
   styleUrls: ['./add-hero.component.scss'],
 })
 export class AddHeroComponent implements OnInit {
-  id!: string;
   formTitle: string = '';
-  heroForm: FormGroup = new FormGroup({
-    name: new FormControl(null, [Validators.required]),
-    description: new FormControl(null, Validators.required),
-    superPower: new FormControl(null, Validators.required),
-  });
+  heroForm: FormGroup = new FormGroup({});
 
   constructor(
+    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private location: Location,
@@ -28,49 +31,38 @@ export class AddHeroComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.params['id'];
-    if (!this.id) {
-      this.formTitle = 'Create new Hero';
-      return;
-    }
-    this.formTitle = 'Edit hero';
-    this.heroesService.getHero(this.id).subscribe(
-      (hero: Hero) => {
-        this.heroForm.patchValue(hero);
-      },
-      (error: HttpErrorResponse) => {
-        //TODO: implementar alertas para que el usuario se entere
-        console.log(error.message);
-      }
-    );
+    this.initializeHeroForm();
   }
 
-  public goBack() {
+  initializeHeroForm(): void {
+    this.heroForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      superPower: ['', [Validators.required]],
+    });
+  }
+
+  goBack() {
     this.location.back();
   }
 
-  public save(): void {
+  save(): void {
     let hero: Hero = {
-      id: this.id ? this.id : '',
-      name: this.heroForm.get('name')?.value,
-      description: this.heroForm.get('description')?.value,
-      superPower: this.heroForm.get('superPower')?.value,
+      id: '',
+      name: this.form.name.value,
+      description: this.form.description.value,
+      superPower: this.form.superPower.value,
     };
 
-    if (this.id) {
-      this.heroesService.editHero(hero).subscribe(
-        (res) => {
-          this.router.navigate(['heroes'], { replaceUrl: true });
-        },
-        (error) => {}
-      );
-      return;
-    }
     this.heroesService.addHero(hero).subscribe(
-      (res) => {
+      (res: ApiResponse) => {
         this.router.navigate(['heroes'], { replaceUrl: true });
       },
       (error) => {}
     );
+  }
+
+  private get form(): { [key: string]: AbstractControl } {
+    return this.heroForm.controls;
   }
 }
