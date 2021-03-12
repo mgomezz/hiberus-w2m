9e988e82585f1cb548a7bd9e4122cc0046ab81ab
Matias Gomez <matias.gomez@valkimia.com>
2021-03-11 23:37:49 -0300

    fixed test for edit and add hero

--- ui/src/app/pages/add-hero/add-hero.component.spec.ts
+++ ui/src/app/pages/add-hero/add-hero.component.spec.ts
@@ -1,16 +1,32 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AddHeroComponent } from './add-hero.component';
 
 describe('AddHeroComponent', () => {
+  let httpTestingController: HttpTestingController;
   let component: AddHeroComponent;
   let fixture: ComponentFixture<AddHeroComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddHeroComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        MatSnackBarModule,
+      ],
+      declarations: [AddHeroComponent],
+    }).compileComponents();
+
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   beforeEach(() => {
--- ui/src/app/pages/edit-hero/edit-hero.component.spec.ts
+++ ui/src/app/pages/edit-hero/edit-hero.component.spec.ts
@@ -1,16 +1,32 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { EditHeroComponent } from './edit-hero.component';
 
 describe('EditHeroComponent', () => {
+  let httpTestingController: HttpTestingController;
   let component: EditHeroComponent;
   let fixture: ComponentFixture<EditHeroComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditHeroComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        MatSnackBarModule,
+      ],
+      declarations: [EditHeroComponent],
+    }).compileComponents();
+
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   beforeEach(() => {
